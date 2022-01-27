bd951347d48ff57f748e80f8e73a3b07cf2ac22e
kaiden <dratsucc@protonmail.com>
2022-01-27 16:47:27 +1100

    added lots of JS for beginnings of validation

--- mytaskplanner/JS/index.js
+++ mytaskplanner/JS/index.js
@@ -9,11 +9,10 @@
 
 // defining current date, 
 // and appending it to '#date' HTML element
-
+let d = new Date().toLocaleDateString('en-us', { 
+	weekday:"long", year:"numeric", month:"short", day:"numeric"});
 let getDate = () => {
 
-	let d = new Date().toLocaleDateString('en-us', { 
-		weekday:"long", year:"numeric", month:"short", day:"numeric"});
 	d = `Today is ${d}.`
 	try {
 		let displayEl = document.querySelector("#date");
@@ -43,27 +42,75 @@ status (must be chosen),
 date (must be the current date or later)
 */
 
-
+// grabbing add task inputs 
 const addName = document.querySelector('#addName');
-const addDesc = document.querySelector('#Description');
-const addStatus = document.querySelector('#Status');
-const addDate = document.querySelector('#Date')
+const addDescription = document.querySelector('#addDescription');
+const addAssignedTo = document.querySelector('#addAssignedTo')
+const addStatus = document.querySelector('#addStatus');
+const addDate = document.querySelector('#addDate')
+/////////////////////////////////////////////
+
+// grabbing edit task inputs
+const editName = document.querySelector('#editName');
+const editDescription = document.querySelector('#editDescription');
+const editAssignedTo = document.querySelector('#editAssignedTo')
+const editStatus = document.querySelector('#editStatus');
+const editDate = document.querySelector('#editDate')
+/////////////////////////////////////////////
+
+// let validator = () => {
+// 	if (addName.value.length >= 5) {
+// 	addName.classList.add('is-valid')
+// 	}
+// }
 
 
+let validateAddForm = () => {
 
-if (addName.value.length >= 5) {
-	document.querySelector()
-}
+	try {
+		if (addName.value >= 5) {
+			addName.classList.add('is-valid');
+			addName.classList.remove('is-invalid');
+		} else {
+			addName.classList.remove('is-valid');
+			addName.classList.add('is-invalid');
+		}
+		if (addDescription.value >= 5) {
+			addDescription.classList.add('is-valid');
+			addDescription.classList.remove('is-invalid');
+		} else {
+			addDescription.classList.remove('is-valid');
+			addDescription.classList.add('is-invalid');
+		}
+		if (addAssignedTo.value >= 5) {
+			addAssignedTo.classList.add('is-valid');
+			addAssignedTo.classList.remove('is-invalid');
+		} else {
+			addAssignedTo.classList.remove('is-valid');
+			addAssignedTo.classList.add('is-invalid');
+		}
+		if (addStatus.value) {
+			addStatus.classList.add('is-valid');
+			addStatus.classList.remove('is-invalid');
+		} else {
+			addStatus.classList.remove('is-valid');
+			addStatus.classList.add('is-invalid');
+		}
+		if (addDate.value < d) {
+			addDate.classList.add('is-valid');
+			addDate.classList.remove('is-invalid');
+		} else {
+			addDate.classList.remove('is-valid');
+			addDate.classList.add('is-invalid');
+		}
 
-
-
-
-//TEST//
-let validateInput = (input) => {
-	if (input.value) {
-		input.classList.add('is-valid');
+	} catch(error) {
+		console.log('Something went wrong. Please try again.')
+		console.log(error)
 	}
 }
+
+
 
 
 
@@ -71,8 +118,47 @@ let validateInput = (input) => {
 // 	FORM SUBMISSION FUNCTION/S	//
 ----------------------------- */ 
 
+// creating new object from taskManager, 
+// which will hold all created tasks
+let taskObj = new taskManager();
 
-validateInput(addName)
+// counter for checking if any invalid inputs were given?
+let invalidCounter = 0;
+
+let addButton = document.querySelector('#addSubmit');
+let editButton = document.querySelector('#editSubmit');
+
+//TEST TASK
+taskObj.addTask('task', 'do balls', 'jeff', 'now', 'todo')
+console.log(taskObj.tasks)
+
+
+// function for assigning values from input form
+// into their appropriate places inside HTML cards
+// -- not sure how it works yet lmao
+let taskMapper = () => {
+	document.createElement()
+}
+
+
+
+
+
+
+let tryAddForm = () => {
+	if (invalidCounter == 0) {
+		try {
+			
+
+
+
+		} catch(error) {
+			console.log(error)
+		}
+	} else {
+		return //?????????????
+	}
+}
 
 
 // let submitForm = () => {
@@ -80,9 +166,11 @@ validateInput(addName)
 // submitButton.addEventListener('click', document.querySelector('#addTask').submit())
 
 
-let test = new taskManager()
-test.addTask('task', 'do balls', 'jeff', 'now', 'todo')
-console.log(test.tasks)
 
 
+addButton.addEventListener('click', validateAddForm())
 
+
+// FUNCTION CALLING //
+
+// validateAddForm()
