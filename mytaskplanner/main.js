ec951363a9892b0fae295a3c10693e03ee91696c
kaiden <dratsucc@protonmail.com>
2022-01-27 15:25:54 +1100

    fiddled with validation

--- mytaskplanner/main.js
+++ mytaskplanner/main.js
@@ -3,9 +3,11 @@
 
 
 
+/* -----------------------------
+// 	DATE DISPLAY FUNCTION	//
+----------------------------- */ 
 
-
-// function for defining current date, 
+// defining current date, 
 // and appending it to '#date' HTML element
 
 let getDate = () => {
@@ -17,6 +19,7 @@ let getDate = () => {
 		let displayEl = document.querySelector("#date");
 		displayEl.innerHTML = d;			
 	} catch(error) {
+		console.log('Uh oh, something went oopsie!')
 		console.log(error)
 	}
 
@@ -26,3 +29,42 @@ let getDate = () => {
 
 //calling date function
 getDate();
+
+
+
+/* -----------------------------
+// 	FORM VALIDATION FUNCTION	//
+----------------------------- */ 
+
+/*
+name (at least 5 characters),
+description (at least 5 characters),
+status (must be chosen),
+date (must be the current date or later)
+*/
+
+
+const formName = document.querySelector('#Name');
+const formDesc = document.querySelector('#Description');
+const formStatus = document.querySelector('#Status');
+const formDate = document.querySelector('#Date')
+
+let validateInput = (input) => {
+	if (formName.value) {
+		input.classList.add('is-valid');
+	}
+}
+
+
+
+/* -----------------------------
+// 	FORM SUBMISSION FUNCTION/S	//
+----------------------------- */ 
+
+
+validateInput(formName)
+
+
+// let submitForm = () => {
+// 	let submitButton = document.querySelector('#submit-button')
+// 	submitButton.addEventListener('click', }
