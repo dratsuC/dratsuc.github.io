065a36e690fdeb5a4bd8d887e2da35e922802b30
kaiden <dratsucc@protonmail.com>
2022-01-27 14:24:05 +1100

    completed date display

--- mytaskplanner/main.js
+++ mytaskplanner/main.js
@@ -1,16 +1,28 @@
 'use strict';
 
-// import default from './JS/date.js';
+
+
+
+
+
+// function for defining current date, 
+// and appending it to '#date' HTML element
 
 let getDate = () => {
 
-	let d = new Date();
+	let d = new Date().toLocaleDateString('en-us', { 
+		weekday:"long", year:"numeric", month:"short", day:"numeric"});
+	d = `Today is ${d}.`
+	try {
+		let displayEl = document.querySelector("#date");
+		displayEl.innerHTML = d;			
+	} catch(error) {
+		console.log(error)
+	}
 
-	let displayEl = document.querySelector("#dateDiv");
-	displayEl.innerHTML = d;	
 
 }
 
 
-
+//calling date function
 getDate();
